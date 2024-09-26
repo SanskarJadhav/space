a6e89ced87aa62c33576ebe86df092b58aefc630
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2024-09-26 22:57:57 +0530

    Update similar.py

--- similar.py
+++ similar.py
@@ -59,8 +59,8 @@ img2 = st.file_uploader("Upload Second Image", type=["jpg", "jpeg", "png"])
 if img1 and img2:
     # Convert the BytesIO objects back to bytes
 
-    image1 = np.array(Image.open(img1))
-    image2 = np.array(Image.open(img2))
+    image1 = np.array(Image.open(img1)).all()
+    image2 = np.array(Image.open(img2)).all()
     
     try:
         if image1 and image2:
